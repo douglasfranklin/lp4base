15895b9f658cbb8a8fe5758211a10db6f79c6935
Douglas Franklin <douglasfkln@outlook.com>
2018-09-20 01:40:21 -0300

    New FeedPage, soma de dois números na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public nome_usuario = "Joaquim da Fonseca";
 
+  public numero_1:number = 0;
+  public numero_2:number = 0;
+  public resultado:number = 0;
+
+  // Quando coloca tipo na variável, você está restringindo o tipo de calor que ela pode receber
+  // public nome_usuario:string = "";
+
+  constructor(public navCtrl: NavController) {
+    // this.resultado = this.somaDoisNumeros(6,8);
+  }
+
+  // public somaDoisNumeros(): void {
+  //   // alert("minha função funcionou");
+  //   alert(5+2);
+  // }
+
+  public somaDoisNumeros(){
+    // return num1+num2;
+    this.resultado = parseFloat((parseFloat(this.numero_1.toString())+parseFloat(this.numero_2.toString())).toFixed(3));
   }
 
 }
