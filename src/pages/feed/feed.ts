a568dcd75c9452b15c1026c514e3fdd29975cdae
Douglas Franklin <douglas.franklin@compasso.com.br>
2018-09-24 14:59:23 -0300

    Array de posts em feeds, style Resultado da Home

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -15,6 +15,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
 
+  public posts = [
+    {
+      name: "Douglas Franklin",
+      profile: "https://ionicframework.com/dist/preview-app/www/assets/img/marty-avatar.png",
+      date: "24 de Setembro de 2018",
+      image: "https://ionicframework.com/dist/preview-app/www/assets/img/advance-card-bttf.png",
+      text: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.",
+      n_likes: 5,
+      n_comment: 4
+    },
+    {
+      name: "Francisco Silva",
+      profile: "https://ionicframework.com/dist/preview-app/www/assets/img/marty-avatar.png",
+      date: "24 de Setembro de 2018",
+      image: "https://ionicframework.com/dist/preview-app/www/assets/img/advance-card-bttf.png",
+      text: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.",
+      n_likes: 5,
+      n_comment: 4
+    },
+    {
+      name: "Adalberto Ferreira",
+      profile: "https://ionicframework.com/dist/preview-app/www/assets/img/marty-avatar.png",
+      date: "24 de Setembro de 2018",
+      image: "https://ionicframework.com/dist/preview-app/www/assets/img/advance-card-bttf.png",
+      text: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.",
+      n_likes: 5,
+      n_comment: 4
+    }
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
